7a871524da6bf7661907049ecb51369f9504adc4
deuk9 <cdy900@gmail.com>
2025-01-30 21:26:04 +0900

    clientOnly 제거 해봄

--- components/content/ProseImg.vue
+++ components/content/ProseImg.vue
@@ -1,24 +1,22 @@
 <template>
-  <ClientOnly>
-    <figure class="flex flex-col items-center">
-      <!-- 이미지 -->
-      <NuxtImg
-        :src="refinedSrc"
-        :alt="alt"
-        :width="width"
-        :height="height"
-
-        class="w-full max-w-lg transition-opacity duration-200"
-      />
-      <!-- 캡션 -->
-      <figcaption
-        v-if="alt"
-        class="text-gray-600 text-sm italic text-center mt-3 mb-2"
-      >
-        {{ alt }}
-      </figcaption>
-    </figure>
-  </ClientOnly>
+  <figure class="flex flex-col items-center">
+    <!-- 이미지 -->
+    <NuxtImg
+      :src="refinedSrc"
+      :alt="alt"
+      :width="width"
+      :height="height"
+      :placeholder="[50, 25, 75, 5]"
+      class="w-full max-w-lg transition-opacity duration-200"
+    />
+    <!-- 캡션 -->
+    <figcaption
+      v-if="alt"
+      class="text-gray-600 text-sm italic text-center mt-3 mb-2"
+    >
+      {{ alt }}
+    </figcaption>
+  </figure>
 </template>
 
 <script setup lang="ts">
@@ -36,11 +34,9 @@ const refinedSrc = computed(() => {
   if (props.src?.startsWith('/') && !props.src.startsWith('//')) {
     const _base = withLeadingSlash(withTrailingSlash(useRuntimeConfig().app.baseURL))
     if (_base !== '/' && !props.src.startsWith(_base)) {
-      console.log('joinURL', joinURL(_base, props.src))
       return joinURL(_base, props.src)
     }
   }
-  console.log('props.src', props.src)
   return props.src
 })
 </script>
