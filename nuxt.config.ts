b43c8a7575e915f95da36b321fa880893ac9c0a8
deuk9 <cdy900@gmail.com>
2025-01-30 23:24:00 +0900

    버그 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,11 +111,11 @@ export default defineNuxtConfig({
     clientBundle: {
       scan: {
         // note that when you specify those values, the default behavior will be overridden
-        globInclude: ['components/**/*.vue', /* ... */],
-        globInclude: ['pages/**/*.vue', /* ... */],
-        globExclude: ['node_modules', 'dist', /* ... */],
+        globInclude: ['components/**/*.vue'],
+        globInclude: ['pages/**/*.vue'],
+        globExclude: ['node_modules', 'dist'],
       },
     },
-
+  },
   target: 'static',
 })
