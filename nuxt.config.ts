7009bee98fc3c2d177200c55c1d244961d39fa1f
deuk9 <cdy900@gmail.com>
2025-01-30 23:41:22 +0900

    icon 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -109,10 +109,12 @@ export default defineNuxtConfig({
 
   icon: {
     clientBundle: {
+      // list of icons to include in the client bundle
+      // scan all components in the project and include icons
+      // scan: true,
       scan: {
         // note that when you specify those values, the default behavior will be overridden
-        globInclude: ['components/**/*.vue'],
-        globInclude: ['pages/**/*.vue'],
+        globInclude: ['components/**/*.vue', 'pages/**/*.vue'],
         globExclude: ['node_modules', 'dist'],
       },
     },
