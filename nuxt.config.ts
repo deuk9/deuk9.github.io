17f394920138c53aa62275926adf03293a310055
deuk9 <cdy900@gmail.com>
2025-01-30 23:21:36 +0900

    icon clien budle 로 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -108,8 +108,14 @@ export default defineNuxtConfig({
   // ssr: false,
 
   icon: {
-    serverBundle: 'local',
-  },
+    clientBundle: {
+      scan: {
+        // note that when you specify those values, the default behavior will be overridden
+        globInclude: ['components/**/*.vue', /* ... */],
+        globInclude: ['pages/**/*.vue', /* ... */],
+        globExclude: ['node_modules', 'dist', /* ... */],
+      },
+    },
 
   target: 'static',
 })
